a72324bf62ab31a9337a678658b9759c9765fe9c
Luis Angulo <builmiguel@gmail.com>
2020-03-12 15:38:30 +0000

    [FE-NGRX] feat: Add Unit Test for app.component, country-display.component, ui-entity-list.component, and country.effects

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'region-country-ngrx'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('region-country-ngrx');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('region-country-ngrx app is running!');
-  });
 });
